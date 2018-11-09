1dc0b9a5fe9081588eed6276cb7623d3899b3929
Jaeyu Jo <ehay@naver.com>
2018-11-10 04:36:10 +0900

    Sopt seminar4 Init

---
+++ src/test/java/org/sopt/sb_seminar4/SbSeminar4ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sopt.sb_seminar4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbSeminar4ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
